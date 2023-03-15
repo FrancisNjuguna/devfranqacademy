874806e63d6de2d5676d1b24a396d77229eec268
FRANCIS NJUGUNA <liperanqey@gmail.com>
2023-03-15 14:58:49 +0300

    update

--- src/App.js
+++ src/App.js
@@ -9,6 +9,30 @@ import Blog from "./components/blog/Blog"
 import Contact from "./components/contact/Contact"
 import Footer from "./components/common/footer/Footer"
 import Home from "./components/home/Home"
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function App() {
+  const { isLoading, error } = useAuth0();
+
+  return (
+    <main className="column">
+      <h1>Auth0 Login</h1>
+      {error && <p>Authentication Error</p>}
+      {!error && isLoading && <p>Loading...</p>}
+      {!error && !isLoading && (
+        <>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </>
+      )}
+    </main>
+  );
+}
+
 function App() {
   return (
     <>
