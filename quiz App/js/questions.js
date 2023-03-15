36bad8b8c73e914e7bf67eb762a1b55240818288
FRANCIS NJUGUNA <liperanqey@gmail.com>
2023-03-15 11:41:52 +0300

    first commit

---
+++ quiz App/js/questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "What does HTML stand for?",
+    choice1: "Hyperlinks and Text Markup Language",
+    choice2: "Hyper Text Markup Language",
+    choice3: "Hyper Text Making Language",
+    choice4: "Hyper Text Mark Language",
+    answer: 1
+},
+            {
+            question: "Which of the following is the correct HTML tag for the largest heading?",
+    choice1: "<h1>",
+    choice2: "<h2>",
+    choice3: "<h5>",
+    choice4: "<h6>",
+    answer: 1
+},
+            {
+            question: "Which HTML tag is used to define an internal style sheet?",
+    choice1: "<script>",
+    choice2: "<style>",
+    choice3: "<html>",
+    choice4: "<svg>",
+    answer: 2
+},
+            {
+            question: "Which tag is used to define a hyperlink?",
+    choice1: "<a>",
+    choice2: "<p>",
+    choice3: "<b>",
+    choice4: "<i>",
+    answer: 1
+},
+            {
+            question: "Which of the following is used to define a list item in HTML?",
+    choice1: "<li>",
+    choice2: "<dl>",
+    choice3: "<ul>",
+    choice4: "<ol>",
+    answer: 1
+},
+            {
+            question: "How do you insert a comment in a HTML file?",
+    choice1: "/*This is Comment*/",
+    choice2: "//This Is Comment",
+    choice3: "<!--- This Is Comment --->",
+    choice4: "//This Is Comment//",
+    answer: 2
+},
+            {
+               question: "Which attribute is used to set the size of an HTML input field?",
+    choice1: "size",
+    choice2: " field-size",
+    choice3: "input-size",
+    choice4: "data-size",
+    answer: 1
+},{
+               question: "Which attribute is used to set the minimum value of an HTML input field?",
+    choice1: "min",
+    choice2: "minimum",
+    choice3: "low",
+    choice4: "start",
+    answer: 1
+},
+            {
+               question: "Inside which HTML element do we put the JavaScript?",
+    choice1: "<js></js>",
+    choice2: "<javascript></javascript>",
+    choice3: "<script></script>",
+    choice4: "<scripting>",
+    answer: 2
+},
+            {
+               question: "Which tag is used to define a button in HTML?",
+    choice1: "<button>",
+    choice2: " <input>",
+    choice3: "<label>",
+    choice4: "<select>",
+    answer: 1
+}];
--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import Header from "./components/common/header/Header"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import About from "./components/about/About"
+import CourseHome from "./components/allcourses/CourseHome"
+import Team from "./components/team/Team"
+import Pricing from "./components/pricing/Pricing"
+import Blog from "./components/blog/Blog"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/common/footer/Footer"
+import Home from "./components/home/Home"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/courses' component={CourseHome} />
+          <Route exact path='/team' component={Team} />
+          <Route exact path='/pricing' component={Pricing} />
+          <Route exact path='/journal' component={Blog} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
